e5837983556f5c16392c0c505bcd339869d474d2
british <angelbritez.m26@gmail.com>
2024-10-28 20:28:38 -0300

    springpepe

---
+++ pepe/src/main/java/com/example/pepe/PepeApplication.java
@@ -0,0 +1,13 @@
+package com.example.pepe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PepeApplication.class, args);
+	}
+
+}
---
+++ pepe/src/test/java/com/example/pepe/PepeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pepe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PepeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
